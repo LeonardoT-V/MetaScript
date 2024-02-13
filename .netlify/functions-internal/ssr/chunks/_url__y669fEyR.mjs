91f18e059470614d7b6b4074a32719c02e01c665
LeonardoT-V <iderltv12@gmail.com>
2024-02-12 22:43:44 -0500

    fix: Hybrid output astro

--- .netlify/functions-internal/ssr/chunks/_url__y669fEyR.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/_url__L03wyHlC.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/url_kLvyJzU8.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/url_t-1yL7qF.mjs');
-
-export { page };
