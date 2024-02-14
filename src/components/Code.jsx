541a647334fbd0d3b2eb7b2dafc26fe2d7b9febe
LeonardoT-V <iderltv12@gmail.com>
2024-02-14 14:42:45 -0500

    fix: preact integration client visible

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -1,19 +1,12 @@
+import { CodeBlock, ocean } from 'react-code-blocks';
+
 export default function Code({
   meta: { tag, type, ...el },
 }) {
-let code = "";
-Object.entries(el).forEach((i) => (code = code + ` ${i[0]}="${i[1]}"`));
+  let code = "";
+  Object.entries(el).forEach((i) => (code = code + ` ${i[0]}="${i[1]}"`));
   return (
-<pre
-        style={{
-          height: '100%',
-          margin: 0,
-          padding: 0,
-          border: 0
-        }}
-        className={'!px-0'}
-      >
-        <code className={`language-html`}>{`<${tag}${code} >`}</code>
-      </pre>
+    <CodeBlock text={`<${tag}${code} >`} language='html' theme={ocean} showLineNumbers={false} className='code_block'
+    />
   )
 }
--- src/components/IconsGalery.jsx
+++ src/components/IconsGalery.jsx
@@ -2,24 +2,23 @@ import Article from "./AR";
 
 export default function IconsGalery({ icons }) {
   return <Article id="icons" title="Galery icons" tag="link">
-  <section class="flex flex-col md:flex-row gap-8 flex-wrap">
-    {
-      icons.map((icon) => (
-        <div class="flex flex-col gap-1 items-center">
-          <img
-            class="rounded max-h-32"
-            src={icon.url}
-            alt={`${icon?.rel} ${icon?.sizes ?? ""}`}
-          />
-          <p class="opacity-50 font-light text-xs">
-            {icon?.rel} {icon.sizes}
-          </p>
-        </div>
-      ))
-    }
-  </section>
-</Article>
-
+    <section class="flex flex-col md:flex-row gap-8 flex-wrap">
+      {
+        icons.map((icon) => (
+          <div class="flex flex-col gap-1 items-center">
+            <img
+              class="rounded max-h-32"
+              src={icon.url}
+              alt={`${icon?.rel} ${icon?.sizes ?? ""}`}
+            />
+            <p class="opacity-50 font-light text-xs">
+              {icon?.rel} {icon.sizes}
+            </p>
+          </div>
+        ))
+      }
+    </section>
+  </Article>
 }
 
 
